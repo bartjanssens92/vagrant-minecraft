36d6a6e720de8aafff5e5bfbf7ca6af79a16d393
bjanssens <bjanssens@inuits.eu>
2014-11-13 14:27:42 +0100

    Initial commit
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a vagrant test box to replicate the lightless server that is running.
+This box serves as testing env for the server.
+This box is puppetized to be able to rebuild the env if you screw up big time.
---
+++ puppet/manifests/nodes/lightless.pp
@@ -0,0 +1,44 @@
+node 'lightless' {
+	
+  $soft=['java-1.7.0-openjdk','nano']
+
+	group { 'sudo':
+    ensure  => 'present',
+  }
+
+  file { '/home/bjanssens':
+    ensure  => 'directory',
+  }
+
+  user { 'bjanssens':
+      ensure   => 'present',
+      home     => '/home/bjanssens',
+      password => 'somethingstronghere',
+      groups   => 'sudo',
+      shell    => '/bin/bash',
+      require  => [
+        Group['sudo'],
+        File['/home/bjanssens'],
+      ]
+  }
+
+  file { '/home/lightlessfilms':
+    ensure  => 'directory',
+  }
+
+  user { 'lightlessfilms':
+      ensure   => 'present',
+      home     => '/home/lightl',
+      password => 'somethingstronghere',
+      groups   => 'sudo',
+      shell    => '/bin/bash',
+      require  => [
+        Group['sudo'],
+        File['/home/lightlessfilms'],
+      ]
+  }
+
+  package{ $soft:
+    ensure => 'installed',
+  }
+}
