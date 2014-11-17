a14c6fd3ba5d42f4ad9ee9c7644e1226c755ba69
bjanssens <bjanssens@inuits.eu>
2014-11-17 10:19:38 +0100

    Added ELK
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- puppet/manifests/nodes/lightless.pp
+++ puppet/manifests/nodes/lightless.pp
@@ -41,4 +41,18 @@ node 'lightless' {
   package{ $soft:
     ensure => 'installed',
   }
+
+  package{ 'elasticsearch-1.4.0.noarch.rpm':
+    provider => 'rpm',
+    ensure   => 'installed',
+    source   => '/vagrant_data/ELK/elasticsearch-1.4.0.noarch.rpm',
+    require  => Package['java-1.7.0-openjdk'],
+  }
+
+  package{ 'logstash-1.4.2-1_2c0f5a1.noarch.rpm':
+    provider => 'rpm',
+    ensure   => 'installed',
+    source   => '/vagrant_data/ELK/logstash-1.4.2-1_2c0f5a1.noarch.rpm',
+    require  => Package['java-1.7.0-openjdk'],
+  }
 }
