ce310021f43805adc264a5c6b7e98de86a7ca888
bjanssens <bjanssens@inuits.eu>
2014-11-24 13:22:41 +0100

    Fixed the ugly exec
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- puppet/manifests/nodes/lightless.pp
+++ puppet/manifests/nodes/lightless.pp
@@ -1,6 +1,6 @@
 node 'lightless' {
 	
-  $soft=['java-1.7.0-openjdk','nano','unzip','lighttpd']
+  $soft=['java-1.7.0-openjdk','nano','unzip','httpd']
 
 	group { 'sudo':
     ensure  => 'present',
@@ -49,22 +49,40 @@ node 'lightless' {
     gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
   }
 
-  package{ $soft:
-    ensure => 'installed',
+  package { $soft:
+    ensure  => 'installed',
     require => Yumrepo['epel.repo'],
   }
 
-  package{ 'elasticsearch-1.4.0.noarch.rpm':
+  package { 'elasticsearch-1.4.0.noarch.rpm':
     provider => 'rpm',
     ensure   => 'installed',
     source   => '/vagrant_data/ELK/elasticsearch-1.4.0.noarch.rpm',
     require  => Package['java-1.7.0-openjdk'],
   }
 
-  package{ 'logstash-1.4.2-1_2c0f5a1.noarch.rpm':
+  package { 'logstash-1.4.2-1_2c0f5a1.noarch.rpm':
     provider => 'rpm',
     ensure   => 'installed',
     source   => '/vagrant_data/ELK/logstash-1.4.2-1_2c0f5a1.noarch.rpm',
     require  => Package['java-1.7.0-openjdk'],
+  }
+
+  # VERY ugly exec to get kibana in the right place
+  exec { 'kibana':
+    path    => '/bin',
+    command => '/bin/rm -rf /var/www/html && \
+      /bin/mkdir /var/www/html && \
+      /usr/bin/unzip /vagrant_data/ELK/kibana-3.1.2.zip -d /var/www/html && \
+      /bin/mv /var/www/html/kibana-3.1.2/* /var/www/html/',
+    require => Package[
+      'unzip',
+      'httpd'
+    ],
+  }
+
+  service { 'httpd':
+    ensure  => 'running',
+    require => Exec['kibana'],
   }
 }
